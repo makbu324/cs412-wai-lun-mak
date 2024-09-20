cf37e68c34eac46ce37f5f440688ec1e7564a3b5
Wai Lun Mak <angusmak@bu.edu>
2024-09-19 23:22:12 -0400

    m

--- cs412/urls.py
+++ cs412/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("hw/", include("hw.urls")), ## we create the URL hw/, 
                                      ## and associate it with URLs in another file
+    path("quotes/", include("quotes.urls")),
 ] + static(settings.STATIC_URL,
            document_root=settings.STATIC_ROOT)
