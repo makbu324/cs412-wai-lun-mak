1bc14ab40b40c2346419eaadd9f1d94a8bdbb4dd
Wai Lun Mak <angusmak@bu.edu>
2024-11-04 13:52:48 -0500

    a9 test v2

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -9,7 +9,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path(r'', views.ShowAllProfilesView.as_view(), name="show_all_profiles"),
     path(r'show_all_profiles', views.ShowAllProfilesView.as_view(), name="show_all_profiles"),
-    # path(r'create_profile', views.CreateProfileView.as_view(), name="create_profile"),
+    path(r'create_profile', views.CreateProfileView.as_view(), name="create_profile"),
     path(r'profile/<int:pk>', views.ShowProfilePageView.as_view(), name="profile"),
     path(r'status/<int:pk>/delete', views.DeleteStatusMessageView.as_view(), name="delete"),
     path(r'status/<int:pk>/update', views.UpdateStatusMessageView.as_view(), name="update_status"),
@@ -21,6 +21,6 @@ urlpatterns = [
     path(r'profile/friend_suggestions', views.ShowFriendSuggestionsView.as_view(), name="friend_suggestions"),
     path(r'profile/news_feed', views.ShowNewsFeedView.as_view(), name="news_feed"),
     path('login/', auth_views.LoginView.as_view(template_name='mini_fb/login.html'), name="login_mini_fb"),
-    path('logout/', auth_views.LogoutView.as_view(next_page='show_all_profiles'), name="logout_mini_fb"), 
+    path('logout/', auth_views.LogoutView.as_view(template_name='mini_fb/logged_out.html'), name="logout_mini_fb"), 
     path('register/', views.RegistrationView.as_view(), name='register_mini_fb'),
 ]
